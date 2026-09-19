30b176bf6c3c27e4e2699e0561e597c7b2b2126f
Maciej Rukat <rukat.maciek@gmail.com>
2026-09-19 16:50:59 +0200

    Framework func declaration

--- VS-Solution/VS-Solution/main.cpp
+++ VS-Solution/VS-Solution/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+
+void Initialize();
+void GetInput();
+void Update();
+void Render();
+void ShutDown();
+
 int main() {
 	cout << "Elo Pomelo" << endl;
 	return 0;
